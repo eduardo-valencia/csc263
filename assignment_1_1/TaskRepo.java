7d1c250f6a8c8cea1d2d0d50cd3a34904314ba7e
EDUARDO VALENCIA <8025918@gmail.com>
2024-07-25 13:28:36 -0700

    Begin adding filter

--- assignment_1_1/TaskRepo.java
+++ assignment_1_1/TaskRepo.java
@@ -4,12 +4,21 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 public class TaskRepo {
   public String getTask() throws IOException {
-    URL url = new URL("https://api.todoist.com/rest/v2/tasks");
+    String filter = "!(@Routine) & !(no date) & !##Clients & !##Freelancing &  !##University & !(@Work)";
+    // String filter = "today";
+    String encodedFilter = URLEncoder.encode(filter, StandardCharsets.UTF_8.toString());
+
+    URL url = new URL("https://api.todoist.com/rest/v2/tasks?filter=" + filter);
     HttpURLConnection connection = (HttpURLConnection) url.openConnection();
     connection.setRequestMethod("GET");
+
+    String token = System.getenv("TODOIST_API_KEY");
+    connection.setRequestProperty("Authorization", "Bearer " + token);
 
     InputStream inputStream = connection.getInputStream();
     BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
@@ -22,6 +31,7 @@ public class TaskRepo {
     }
 
     bufferedReader.close();
+    connection.disconnect();
     return responseBuilder.toString();
   }
 }
