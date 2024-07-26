bd71de14eb48c6af0046061a6ab898aac9d50296
EDUARDO VALENCIA <8025918@gmail.com>
2024-07-26 12:38:35 -0700

    Add tasks with limit, refactor code, show class names

--- assignment_1_1/Main.java
+++ assignment_1_1/Main.java
@@ -5,13 +5,34 @@ public class Main extends MetaClass {
     super("Main");
   }
 
-  public static void main(String[] args) throws IOException {
-    TaskRepo taskRepo = new TaskRepo();
-    Task[] tasks = taskRepo.getTasks();
-
+  private static void showTasks(Task[] tasks) {
+    System.out.println("Tasks:");
     for (int index = 0; index < tasks.length; index++) {
       Task task = tasks[index];
       System.out.println(task.name);
     }
+    System.out.println("\n");
+  }
+
+  private static void showClass(MetaClass classInstance) {
+    System.out.println("Class name: " + classInstance.displayName);
+  }
+
+  private static void showAFewClassNames(TaskRepo taskRepo, Task[] tasks) {
+    System.out.println("Classes:");
+    showClass(tasks[0]);
+    showClass(taskRepo);
+  }
+
+  public static void main(String[] args) throws IOException {
+    TaskRepo taskRepo = new TaskRepo();
+
+    Task[] tasks = taskRepo.getTasks();
+    showTasks(tasks);
+
+    Task[] tasksWithLimit = taskRepo.getTasks(1);
+    showTasks(tasksWithLimit);
+
+    showAFewClassNames(taskRepo, tasks);
   }
 }
--- assignment_1_1/TaskRepo.java
+++ assignment_1_1/TaskRepo.java
@@ -1,10 +1,33 @@
 import java.time.LocalDate;
 
-public class TaskRepo {
+public class TaskRepo extends MetaClass {
+  public TaskRepo() {
+    super("Task repo");
+  }
+
   public Task[] getTasks() {
     Task task1 = new Task("Task 1", LocalDate.now());
     Task task2 = new Task("Task 2", LocalDate.now());
     Task[] tasks = { task1, task2 };
+    return tasks;
+  }
+
+  private int getLowerLimit(int tasksLength, int limit) {
+    if (tasksLength > limit)
+      return limit;
+    return tasksLength;
+  }
+
+  public Task[] getTasks(int limit) {
+    Task[] allTasks = getTasks();
+    int lowerLimit = getLowerLimit(allTasks.length, limit);
+
+    Task[] tasks = new Task[lowerLimit];
+
+    for (int index = 0; index < lowerLimit; index++) {
+      tasks[index] = allTasks[index];
+    }
+
     return tasks;
   }
 }
