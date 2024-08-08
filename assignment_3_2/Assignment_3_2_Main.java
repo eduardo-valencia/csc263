b1fd1b0e928c712274401274a1bded40612ba515
EDUARDO VALENCIA <8025918@gmail.com>
2024-08-08 12:26:51 -0700

    Fix test not working

---
+++ assignment_3_2/Assignment_3_2_Main.java
@@ -0,0 +1,14 @@
+import java.util.LinkedList;
+
+public class Assignment_3_2_Main {
+  public LinkedList<String> linkedList = new LinkedList<>();
+
+
+  // TODO: Don't create any data structures until professor tells us what we
+  // have to do.
+  public static void main(String[] args) {
+    Assignment_3_2_Main mainClass = new Assignment_3_2_Main();
+    System.out.println("Successfully created data structures.");
+  }
+
+}
--- assignment_3_2/LinkedListTest.java
+++ assignment_3_2/LinkedListTest.java
@@ -3,10 +3,12 @@ import static org.junit.jupiter.api.Assertions.*;
 class LinkedListTest {
 
     @org.junit.jupiter.api.Test
-    @DisplayName("It inserts the node")
+    @org.junit.jupiter.api.DisplayName("It inserts the node")
     void insertAtHead() {
-        Node node = new Node("potato", null);
-        LinkedList linkedList = new LinkedList(node);
-        assertEquals(linkedList.node, node);
+        Assignment_3_2_Main main = new Assignment_3_2_Main();
+        String value = "Test";
+        main.linkedList.push(value);
+        String firstNode = main.linkedList.getFirst();
+        assertEquals(firstNode, value);
     }
 }
