0baa53a660ea019e564425ec9dd7ea4c98ebe4db
EDUARDO VALENCIA <8025918@gmail.com>
2024-08-08 12:33:25 -0700

    Test getting first item

--- assignment_3_2/Assignment_3_2_Main.java
+++ assignment_3_2/Assignment_3_2_Main.java
@@ -3,6 +3,10 @@ import java.util.LinkedList;
 public class Assignment_3_2_Main {
   public LinkedList<String> linkedList = new LinkedList<>();
 
+  public Assignment_3_2_Main() {
+//    this.linkedList.add("Unknown");
+  }
+
 
   // TODO: Don't create any data structures until professor tells us what we
   // have to do.
--- assignment_3_2/LinkedListTest.java
+++ assignment_3_2/LinkedListTest.java
@@ -3,12 +3,24 @@ import static org.junit.jupiter.api.Assertions.*;
 class LinkedListTest {
 
     @org.junit.jupiter.api.Test
-    @org.junit.jupiter.api.DisplayName("It inserts the node")
-    void insertAtHead() {
+    @org.junit.jupiter.api.DisplayName("It inserts the item at the beginning")
+    public void addFirst() {
         Assignment_3_2_Main main = new Assignment_3_2_Main();
         String value = "Test";
-        main.linkedList.push(value);
+        main.linkedList.addFirst(value);
         String firstNode = main.linkedList.getFirst();
         assertEquals(firstNode, value);
+    }
+
+    @org.junit.jupiter.api.Test
+    @org.junit.jupiter.api.DisplayName("It gets the first item, even when there are multiple items")
+    public void getFirst() {
+        Assignment_3_2_Main main = new Assignment_3_2_Main();
+        String firstValue = "Test";
+        main.linkedList.addFirst("Value 2");
+        main.linkedList.addFirst(firstValue);
+
+        String firstNode = main.linkedList.getFirst();
+        assertEquals(firstNode, firstValue);
     }
 }
