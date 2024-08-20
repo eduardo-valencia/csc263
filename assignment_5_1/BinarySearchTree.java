c91b3a1a5d50055ac7e59af20cd21d26af02b3bc
EDUARDO VALENCIA <8025918@gmail.com>
2024-08-20 12:28:28 -0700

    Add find, print methods

--- assignment_5_1/BinarySearchTree.java
+++ assignment_5_1/BinarySearchTree.java
@@ -8,4 +8,32 @@ public class BinarySearchTree<Value extends Comparable<Value>> {
     else
       this.mainNode = newNode;
   }
+
+  public Node<Value> find(Value value) {
+    Node<Value> nodeToSearch = new Node<Value>(value);
+
+    while (nodeToSearch != null) {
+      int diff = nodeToSearch.value.compareTo(value);
+      if (diff == 0)
+        return nodeToSearch;
+      else if (diff < 0)
+        nodeToSearch = nodeToSearch.leftNode;
+      else
+        nodeToSearch = nodeToSearch.rightNode;
+    }
+
+    return nodeToSearch;
+  }
+
+  private void printFromNode(Node<Value> node) {
+    if (node == null)
+      return;
+    System.out.println("Value: " + node.value.toString());
+    this.printFromNode(node.leftNode);
+    this.printFromNode(node.rightNode);
+  }
+
+  public void showValues() {
+    this.printFromNode(mainNode);
+  }
 }
