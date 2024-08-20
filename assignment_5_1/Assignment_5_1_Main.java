db69bacf09b1a5e386ee2d78161143361165c2c8
EDUARDO VALENCIA <8025918@gmail.com>
2024-08-20 12:46:43 -0700

    Improve UI

--- assignment_5_1/Assignment_5_1_Main.java
+++ assignment_5_1/Assignment_5_1_Main.java
@@ -1,5 +1,7 @@
 public class Assignment_5_1_Main {
   private static void testAdd() {
+    System.out.println("The following shows adding values:");
+
     BinarySearchTree<Integer> bst = new BinarySearchTree<>();
 
     bst.addValue(3);
@@ -7,22 +9,28 @@ public class Assignment_5_1_Main {
     bst.addValue(8);
 
     bst.showValues();
+
+    System.out.println("");
   }
 
   private static void testFind() {
+    System.out.println("The following shows the result of finding the value 8:");
+
     BinarySearchTree<Integer> bst = new BinarySearchTree<>();
 
     Integer lastValue = 8;
-    bst.addValue(3);
-    bst.addValue(10);
+    bst.addValue(1);
+    bst.addValue(6);
     bst.addValue(lastValue);
 
     Node<Integer> nodeWithValue = bst.find(lastValue);
-    System.out.println("Value: " + nodeWithValue);
+    System.out.println("Successfully found a node with a value of " + nodeWithValue.value.toString() + ".");
+
+    System.out.println("");
   }
 
   public static void main(String[] args) {
     testAdd();
-    // testFind();
+    testFind();
   }
 }
