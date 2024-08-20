024b815ca8dfff1fd32ceeb4a002f84eaf71413a
EDUARDO VALENCIA <8025918@gmail.com>
2024-08-20 13:41:11 -0700

    Add comments

--- assignment_5_2/Assignment_5_2_Main.java
+++ assignment_5_2/Assignment_5_2_Main.java
@@ -1,13 +1,31 @@
 public class Assignment_5_2_Main {
+  /**
+   * Starts a thread
+   */
   private static void startThread() {
+    /**
+     * Creates a task
+     */
     Task task = new Task();
+    /**
+     * Creates a thread
+     */
     Thread myThread = new Thread(task);
+    /**
+     * Starts the thread
+     */
     myThread.start();
   }
 
+  /**
+   * The program's starting point
+   */
   public static void main(String[] args) {
     System.out.println("Creating threads!\n");
 
+    /**
+     * Starts multiple threads to show that threads can run simultaneously
+     */
     startThread();
     startThread();
   }
--- assignment_5_2/Task.java
+++ assignment_5_2/Task.java
@@ -1,17 +1,39 @@
+/**
+ * A task that counts to 20.
+ */
 public class Task implements Runnable {
+  /**
+   * Counts up to 20
+   */
   private void showCountUp() {
     int count = 0;
 
     while (count <= 20) {
+      /**
+       * Gets the thread's name
+       */
       String name = Thread.currentThread().getName();
+      /**
+       * Shows the thread's name along with the current count to show what
+       * thread the count belongs.
+       */
       System.out.println(name + ": " + count);
       count++;
     }
   }
 
+  /**
+   * The starting point for the task.
+   */
   public void run() {
     String name = Thread.currentThread().getName();
+    /**
+     * Shows that the thread started
+     */
     System.out.println("Running task in thread " + name + ". Beginning count to 20.\n");
+    /**
+     * Starts counting up
+     */
     this.showCountUp();
   }
 }
