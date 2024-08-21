5d24005d5ed23187189828e60705e363a938b229
EDUARDO VALENCIA <8025918@gmail.com>
2024-08-21 13:40:21 -0700

    Add generation functionality

--- final/TaskRepo.java
+++ final/TaskRepo.java
@@ -10,34 +10,49 @@ public class TaskRepo {
     return today.datesUntil(endDate);
   }
 
-  private ArrayList<ToDoTask> list() {
+  public ArrayList<ToDoTask> list() {
     return new ArrayList<ToDoTask>();
+  }
+
+  private void writeTasks(ArrayList<ToDoTask> tasks) {
+
   }
 
   private ToDoTask create(ToDoTask fields) {
     ArrayList<ToDoTask> tasks = this.list();
-    return fields;
+
+    Task task = fields.clone();
+    task.id = tasks.length;
+
+    tasks.push(task);
+
+    this.writeTasks(tasks);
+
+    return task;
   }
 
-  private ToDoTask generate() {
-    ToDoTask task = new ToDoTask();
-    task.id = 
+  private ToDoTask generateForDate(int index, LocalDate date) {
+    ToDoTask fields = new ToDoTask();
+    fields.name = "Task " + index;
+    fields.dueDate = date;
+    return this.create(fields);
   }
 
-  private ArrayList<ToDoTask> createTasksForDate(Iterator<LocalDate> iterator, int index) {
+  private void createTasksForDate(Iterator<LocalDate> iterator, int index) {
     LocalDate date = iterator.next();
     int tasksToGenerate = 4 - index;
     for (int count = 0; count < tasksToGenerate; count++) {
-
+      this.generateForDate(index, date);
     }
   }
 
-  private ArrayList<ToDoTask> generateBatch() {
+  public void generateBatch() {
     Stream<LocalDate> dates = this.getDatesToGenTasksFor();
     Iterator<LocalDate> iterator = dates.iterator();
-
     int index = 0;
+
     while (iterator.hasNext()) {
+      this.createTasksForDate(iterator, index);
       index++;
     }
   }
--- final/ToDoTask.java
+++ final/ToDoTask.java
@@ -4,4 +4,12 @@ public class ToDoTask {
   public int id;
   public String name;
   public LocalDate dueDate;
+
+  public ToDoTask clone() {
+    ToDoTask task = new ToDoTask();
+    task.id = this.id;
+    task.name = this.name;
+    task.dueDate = this.dueDate;
+    return task;
+  }
 }
