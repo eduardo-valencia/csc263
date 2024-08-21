86148354f4b97ec0f934dd12ea5bb078095775b9
EDUARDO VALENCIA <8025918@gmail.com>
2024-08-21 13:32:26 -0700

    Convert factory into repo

--- final/TaskRepo.java
+++ final/TaskRepo.java
@@ -1,12 +1,22 @@
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.stream.Stream;
 
-public class TaskFactory {
+public class TaskRepo {
   private Stream<LocalDate> getDatesToGenTasksFor() {
     LocalDate today = LocalDate.now();
     LocalDate endDate = today.plusDays(3);
     return today.datesUntil(endDate);
+  }
+
+  private ArrayList<ToDoTask> list() {
+    return new ArrayList<ToDoTask>();
+  }
+
+  private ToDoTask create(ToDoTask fields) {
+    ArrayList<ToDoTask> tasks = this.list();
+    return fields;
   }
 
   private ToDoTask generate() {
@@ -14,7 +24,7 @@ public class TaskFactory {
     task.id = 
   }
 
-  private ToDoTask[] createTasksForDate(Iterator<LocalDate> iterator, int index) {
+  private ArrayList<ToDoTask> createTasksForDate(Iterator<LocalDate> iterator, int index) {
     LocalDate date = iterator.next();
     int tasksToGenerate = 4 - index;
     for (int count = 0; count < tasksToGenerate; count++) {
@@ -22,7 +32,7 @@ public class TaskFactory {
     }
   }
 
-  private ToDoTask[] generateBatch() {
+  private ArrayList<ToDoTask> generateBatch() {
     Stream<LocalDate> dates = this.getDatesToGenTasksFor();
     Iterator<LocalDate> iterator = dates.iterator();
 
